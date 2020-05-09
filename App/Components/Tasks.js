717f55089bf65a303d14b7fbb7af9844c83cad8b
Joshua Ramirez <joshua.ramirez@me.com>
2020-05-09 12:10:56 -0500

    Setting all values in initializer.

--- App/Components/Tasks.js
+++ App/Components/Tasks.js
@@ -2,41 +2,42 @@ import {ComponentNames} from "../Core/ComponentNames.js";
 import {DomFactory} from "../Core/DomFactory.js";
 import {Task} from "./Task.js";
 
-export const Tasks = function() {
+export const Tasks = function () {
   let id;
   let element;
   let parent;
   let addButton;
   let unOrderedList;
-  let tasks = [];
+  let tasks;
   let api;
   let taskCounter;
-  const initialize = function() {
+  const initialize = function () {
     id = ComponentNames.Tasks;
+    tasks = [];
     taskCounter = 0;
   };
-  const subscribe = function() {
-    addButton.addEventListener('click',onAddTaskClicked);
+  const subscribe = function () {
+    addButton.addEventListener('click', onAddTaskClicked);
   };
-  const onAddTaskClicked = function() {
+  const onAddTaskClicked = function () {
     Add();
   };
-  const Add = function() {
+  const Add = function () {
     const index = taskCounter;
     const task = Task(index);
     task.Render(adoptTask);
     tasks.push(task);
     taskCounter++;
   };
-  const adoptTask = function(task, taskElement) {
+  const adoptTask = function (task, taskElement) {
     unOrderedList.appendChild(taskElement);
     return element;
   }
-  const Remove = function(index, element) {
+  const Remove = function (index, element) {
     tasks.splice(index, 1);
     unOrderedList.removeChild(element);
   }
-  const Render = function(acceptAdoption) {
+  const Render = function (acceptAdoption) {
     element = DomFactory.Divider(id);
     unOrderedList = DomFactory.UnOrderedList();
     addButton = DomFactory.Button('New Task');
